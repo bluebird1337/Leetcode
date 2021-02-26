a63ac83db8505a3c35611a92399912fbd44ca1b5
Wang Hong Wen <whwen0528@gmail.com>
2021-02-26 16:40:22 +0800

    2/25 questions

---
+++ Feburary/2.21/2.21.cpp
@@ -0,0 +1,19 @@
+﻿// https://leetcode.com/problems/broken-calculator/
+
+#include <iostream>
+#include<queue>
+using namespace std;
+
+class Solution {
+public:
+    int brokenCalc(int X, int Y) {
+        int res = 0;
+        while (X < Y) {
+            res += 1 + (Y % 2);
+            if (Y % 2) Y += 2;
+            Y /= 2;
+        }
+        return res + (X-Y);
+    }
+};
+
---
+++ Feburary/2.23/2.23.cpp
@@ -0,0 +1,47 @@
+﻿// 
+
+#include <iostream>
+#include<vector>
+#include<algorithm>
+#include <map>
+using namespace std;
+
+
+class Solution {
+public:
+    bool canForm(string s, string ss) {
+        int tmp = 0;
+        for (auto c : s) {
+            if (c == ss[tmp])
+                tmp++;
+        }
+        return tmp == ss.size();
+    }
+
+    string findLongestWord(string s, vector<string>& d) {
+        map<char, vector<string>>mp;
+        for (string dic : d) {
+            char first = dic[0];
+            mp[first].push_back(dic);
+        }
+        for (auto iter = mp.begin(); iter != mp.end(); iter++) {
+            sort(iter->second.begin(), iter->second.end(), [](string a, string b) {return a.size() < b.size(); });
+        }
+        int len = 0;
+        string res = "";
+        string alphabet = "abcdefghijklmnopqrstuvwxyz";
+        for (char c : alphabet) {
+            if (mp.find(c) == mp.end()) continue;
+            vector<string>tmp = mp[c];
+            for (auto iter = tmp.rbegin(); iter != tmp.rend(); iter++) {
+                if ((*iter).length() <= len) break;
+                if (canForm(s, *iter)) {
+                    res = *iter;
+                    len = (*iter).length();
+                }
+            }
+        }
+        return res;
+    }
+};  
+
